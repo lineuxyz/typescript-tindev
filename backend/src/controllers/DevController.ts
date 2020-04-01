4f8a904d6cd5b5c9ba0545e6b5ad156df982a7f0
Lineu Pastorelli <lineualberto@gmail.com>
2020-04-01 10:32:37 -0300

    adicionado controller de devs

--- backend/src/controllers/DevController.ts
+++ backend/src/controllers/DevController.ts
@@ -1,9 +1,45 @@
-import axios from 'axios'
+import Axios, { Request, Response } from 'axios'
+import Dev from '../models/Dev'
 
-export default function store (req, res) {
-  const { username } = req.body
+class DevController {
+  public async index (req: Request, res: Response): Promise<Response> {
+    const { user } = req.headers
 
-  const response = axios.get(`https://api.github.com/users/${username}`)
+    const loggedDev = await Dev.findById(user)
 
-  return res.json(response.data)
+    const users = await Dev.find({
+      $and: [
+        { _id: { $ne: user } },
+        { _id: { $nin: loggedDev.likes } },
+        { _id: { $nin: loggedDev.deslikes } }
+      ]
+    })
+
+    return res.json(users)
+  }
+
+  public async store (req: Request, res: Response): Promise<Response> {
+    const { username } = req.body
+
+    const userExists = await Dev.findOne({ user: username })
+
+    if (userExists) {
+      return res.json(userExists)
+    }
+
+    const response = await Axios.get(`https://api.github.com/users/${username}`)
+
+    const { name, bio, avatar_url: avatar } = response.data
+
+    const dev = await Dev.create({
+      name,
+      user: username,
+      bio,
+      avatar
+    })
+
+    return res.json(dev)
+  }
 }
+
+export default new DevController()
